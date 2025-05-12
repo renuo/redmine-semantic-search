9fd27c90a8aa1da5c2f141e8d779c58f2bccab4e
Sami Hindi <sami.hindi@renuo.ch>
2025-05-12 08:59:35 +0200

    Fix linting

--- init.rb
+++ init.rb
@@ -1,44 +1,44 @@
-require 'logger'
+require "logger"
 
-require 'dotenv/load'
-require 'ruby/openai'
+require "dotenv/load"
+require "ruby/openai"
 
-require 'pathname'
+require "pathname"
 plugin_root = Pathname.new(__FILE__).dirname
-lib_dir = plugin_root.join('lib')
+lib_dir = plugin_root.join("lib")
 $LOAD_PATH.unshift(lib_dir.to_s) unless $LOAD_PATH.include?(lib_dir.to_s)
 
-require_dependency 'semantic_search/issue_hooks'
-require_dependency 'semantic_search/hooks/view_hooks'
+require_dependency "semantic_search/issue_hooks"
+require_dependency "semantic_search/hooks/view_hooks"
 
 Redmine::Plugin.register :semantic_search do
-  name 'Semantic Search'
-  author 'Sami Hindi @ Renuo'
-  description 'This redmine plugin allows you to search issues using natural language, by storing the issue content in a vector database.'
-  version '0.0.1'
-  url 'https://github.com/renuo/redmine-semantic-search'
-  author_url 'https://github.com/renuo'
+  name "Semantic Search"
+  author "Sami Hindi @ Renuo"
+  description "This redmine plugin allows you to search issues using natural language, by storing the issue content in a vector database."
+  version "0.0.1"
+  url "https://github.com/renuo/redmine-semantic-search"
+  author_url "https://github.com/renuo"
 
   settings default: {
-    'base_url' => 'https://api.openai.com/v1',
-    'embedding_model' => 'text-embedding-ada-002',
-    'search_limit' => 25,
-    'include_description' => '1',
-    'include_comments' => '1',
-    'include_time_entries' => '1'
-  }, partial: 'settings/semantic_search_settings'
+    "base_url" => "https://api.openai.com/v1",
+    "embedding_model" => "text-embedding-ada-002",
+    "search_limit" => 25,
+    "include_description" => "1",
+    "include_comments" => "1",
+    "include_time_entries" => "1"
+  }, partial: "settings/semantic_search_settings"
 
   menu :top_menu, :semantic_search,
-       { controller: 'semantic_search', action: 'index' },
+       { controller: "semantic_search", action: "index" },
        caption: :label_semantic_search,
        if: Proc.new { User.current.logged? && User.current.allowed_to?(:use_semantic_search, nil, global: true) }
 
   menu :admin_menu, :semantic_search,
-       { controller: 'semantic_search', action: 'settings' },
+       { controller: "semantic_search", action: "settings" },
        caption: :label_semantic_search_settings
 
   menu :application_menu, :sync_embeddings,
-       { controller: 'semantic_search', action: 'sync_embeddings' },
+       { controller: "semantic_search", action: "sync_embeddings" },
        caption: :button_sync_embeddings,
        html: { method: :post },
        if: Proc.new { User.current.admin? }
