c194bc58d8ebe75ccfa87c5a7e3318e8108d4f31
Sami Hindi <sami.hindi@renuo.ch>
2025-05-19 11:20:30 +0200

    Improve COntroller

--- app/controllers/redmine_semantic_search_controller.rb
+++ app/controllers/redmine_semantic_search_controller.rb
@@ -8,8 +8,10 @@ class RedmineSemanticSearchController < ApplicationController
     @projects = Project.visible.sorted.to_a
     @question = params[:q] || ""
     @results = []
+    @search_performed = false
 
     if @question.present?
+      @search_performed = true
       search_service = RedmineSemanticSearchService.new
       search_limit = Setting.plugin_redmine_semantic_search["search_limit"].to_i
       @results = search_service.search(@question, User.current, search_limit)
