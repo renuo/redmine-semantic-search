e0680604d1ee7ad85826f72ddc4010230c6c49df
Sami Hindi <sami.hindi@renuo.ch>
2025-05-21 12:11:21 +0200

    Revert "Improve test case"
    This reverts commit 303eed2f3ef68be886f784a40d0b77e029dbb5fe.

--- test/system/redmine_semantic_search_system_test.rb
+++ test/system/redmine_semantic_search_system_test.rb
@@ -96,7 +96,7 @@ class RedmineSemanticSearchSystemTest < ApplicationSystemTestCase
       click_button 'Search', wait: 3
     end
 
-    assert page.has_css?('p.nodata, div.nodata, .nodata, #content:contains("No data")', wait: 5)
+    assert_selector 'p.nodata', wait: 3
   end
 
   test "semantic search page is accessible only to authorized users" do
